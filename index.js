ae9662615d108ea8041c619d05074b7cf3d1ea84
willkimkr <98241898+willkimkr@users.noreply.github.com>
2024-07-03 13:46:34 +0000

    endpoint create

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ app.get('/oi', function (req, res) {
   res.send('ola mundo!')
 })
 
+//lista de personagens
+const lista = ["rick sanches", "morty smith", "summer smith"]
+
+// read all- [get] /item
+app.get('/item', function (req, res) {
+  res.send(lista)
+})
+
+
+// sinalizar para o express que vamos usar json no boby
+app.use(express.json())
+
+//create - [post] /item
+app.post('/item', function (req, res){
+  console.log(req.body)
+  res.send('create')
+})
+
+//
+
 app.listen(3000)
