cc7616772787db60b05d49f04f3fd4aaa0287f92
willkimkr <98241898+willkimkr@users.noreply.github.com>
2024-07-03 11:37:18 +0000

    nodemon instalado

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
 app.get('/oi', function (req, res) {
-  res.send('ola mundo')
+  res.send('ola mundo!')
 })
 
 app.listen(3000)
